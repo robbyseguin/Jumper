4ae23c52ffc2a461323df3a23b473b085d74a93a
robbyseguin <robbyseguin@gmail.com>
2023-09-25 14:36:34 +0200

    Beginning of Project
    *Added initialization of window for game.
    *Added Test

--- jumper.cpp
+++ jumper.cpp
@@ -3,4 +3,22 @@
 int main()
 {
 
+// Screen Size
+const int screenWidth = 800;
+const int screenHeight = 600;
+
+// Window init
+InitWindow(screenWidth, screenHeight, "Jumper");
+SetTargetFPS(60);
+
+while(!WindowShouldClose())
+{   
+    //Start Drawing
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+
+    //End Drawing
+    EndDrawing();
+}    
+
 }
