b29999eab8118f8b588d89a5a4bcdc67ecc3d1b2
robbyseguin <robbyseguin@gmail.com>
2023-09-25 19:02:22 +0200

    Adding Textures to Game
    *Added Player Texture

--- jumper.cpp
+++ jumper.cpp
@@ -7,6 +7,28 @@ int main()
 const int screenWidth = 800;
 const int screenHeight = 600;
 
+//Player Variables
+const int playerJumpHeight = 25;
+
+//Player Texture
+Texture2D player = LoadTexture("textures/scarfy.png");
+Rectangle playerRect;
+playerRect.width = player.width / 6;
+playerRect.height = player.height;
+playerRect.x = 0;
+playerRect.y = 0;
+Vector2 playerPos;
+playerPos.x = screenWidth / 2 - playerRect.width / 2;
+playerPos.y = screenHeight - playerRect.height;
+
+
+// Game Engine Physics Variables
+int velocity = 0;
+int gravity = 1;
+
+// is Conditions
+bool isJumping = false;
+
 // Window init
 InitWindow(screenWidth, screenHeight, "Jumper");
 SetTargetFPS(60);
@@ -16,9 +38,33 @@ while(!WindowShouldClose())
     //Start Drawing
     BeginDrawing();
     ClearBackground(RAYWHITE);
+    
+    if(playerRect.y >= screenHeight - playerRect.height)
+    {   
+        // stop velocity
+        velocity = 0;
+        isJumping = false;
+    }
+    else
+    {
+    // update gravity
+    velocity += gravity;
+    isJumping = true;
+    }
+    
+     if(IsKeyDown(KEY_SPACE) && !isJumping)
+    {
+        velocity -= playerJumpHeight;
+    }
+
+    // update Y position
+    playerPos.y += velocity;
+    DrawTextureRec(player, playerRect, playerPos, WHITE);
 
     //End Drawing
     EndDrawing();
-}    
+}
+UnloadTexture(player);
+CloseWindow();  
 
 }
